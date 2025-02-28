3506d0e8373a8234f6796dc557e9ddce686fad1a
molgergo01 <72103331+molgergo01@users.noreply.github.com>
2025-02-28 22:06:34 +0100

    Create PG Schema and Repository for transaction and account (#2)

---
+++ src/main/java/com/molgergo01/finance/backend/model/Account.java
@@ -0,0 +1,18 @@
+package com.molgergo01.finance.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(schema = "finance", name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private Long balance;
+}
---
+++ src/main/java/com/molgergo01/finance/backend/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.molgergo01.finance.backend.repository;
+
+import com.molgergo01.finance.backend.model.Transaction;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends CrudRepository<Transaction, UUID> {
+}
