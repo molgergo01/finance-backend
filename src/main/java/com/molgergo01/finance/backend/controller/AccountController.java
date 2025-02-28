f6f113b41c05d4199d4caed57402e78edae61892
molgergo01 <72103331+molgergo01@users.noreply.github.com>
2025-02-28 23:02:49 +0100

    Create GET "/api/v1/account/{id}/balance" endpoint (#3)

---
+++ src/main/java/com/molgergo01/finance/backend/controller/AccountController.java
@@ -0,0 +1,25 @@
+package com.molgergo01.finance.backend.controller;
+
+import com.molgergo01.finance.backend.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/account")
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountService accountService;
+
+    @GetMapping(value = "/{id}/balance", produces = "application/json")
+    public ResponseEntity<Long> getBalance(@PathVariable final UUID id) {
+        final Long balance = accountService.findBalanceById(id);
+
+        return ResponseEntity.ok(balance);
+    }
+}
