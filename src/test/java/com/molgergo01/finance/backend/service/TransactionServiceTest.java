1fa31efb84c6138c55588f4bd3849ed108940134
molgergo01 <mol.gergo01@gmail.com>
2025-03-01 14:50:02 +0100

    Add validation to APIs

--- src/test/java/com/molgergo01/finance/backend/service/TransactionServiceTest.java
+++ src/test/java/com/molgergo01/finance/backend/service/TransactionServiceTest.java
@@ -2,6 +2,7 @@ package com.molgergo01.finance.backend.service;
 
 import com.molgergo01.finance.backend.model.entity.Transaction;
 import com.molgergo01.finance.backend.repository.TransactionRepository;
+import com.molgergo01.finance.backend.validator.TransactionValidator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,8 @@ import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class TransactionServiceTest {
+    @MockitoBean
+    private TransactionValidator transactionValidator;
     @MockitoBean
     private TransactionRepository transactionRepositoryMock;
     @MockitoBean
@@ -32,6 +35,7 @@ class TransactionServiceTest {
 
         objectUnderTest.makeTransaction(transaction);
 
+        verify(transactionValidator).validateOnMakeTransaction(transaction);
         verify(transactionRepositoryMock).save(transaction);
         verify(accountServiceMock).subtractBalance(UUID_1, expectedAmount);
         verify(accountServiceMock).addBalance(UUID_2, expectedAmount);
