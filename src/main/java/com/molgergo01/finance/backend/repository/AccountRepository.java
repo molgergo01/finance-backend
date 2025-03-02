625a764bdffec4b15b7fe544151b60a0cb3a9946
molgergo01 <72103331+molgergo01@users.noreply.github.com>
2025-03-02 02:52:21 +0100

    Replace update queries with JPA save and fix concurrency (#17)

--- src/main/java/com/molgergo01/finance/backend/repository/AccountRepository.java
+++ src/main/java/com/molgergo01/finance/backend/repository/AccountRepository.java
@@ -1,7 +1,8 @@
 package com.molgergo01.finance.backend.repository;
 
 import com.molgergo01.finance.backend.model.entity.Account;
-import org.springframework.data.jpa.repository.Modifying;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,16 +11,9 @@ import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account, UUID> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Account findAccountById(UUID id);
 
     @Query("SELECT a.balance FROM Account a WHERE a.id = :id")
     Long findBalanceById(final UUID id);
-
-    @Modifying
-    @Query(value = "UPDATE accounts SET balance = balance + :amount WHERE id = :id", nativeQuery = true)
-    void addBalanceById(final UUID id, final Long amount);
-
-    @Modifying
-    @Query(value = "UPDATE accounts SET balance = balance - :amount WHERE id = :id AND balance >= :amount", nativeQuery = true)
-    void subtractBalanceById(final UUID id, final Long amount);
 }
